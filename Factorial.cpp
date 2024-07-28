c631e35c1e3938a83d7c0533ca6acfa4ee488ecc
Abdullah Shoab <imshoab883@gmail.com>
2024-07-28 21:22:47 +0600

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+
+    if(n == 0){
+        return 1;
+    }
+
+    int small = factorial(n-1);
+    int big = n* small;
+
+    return big;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the Value of n"<<" ";
+    cin>>n;
+
+    cout<<factorial(n);
+    
+    return 0;
+
+}
